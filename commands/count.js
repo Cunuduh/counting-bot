ae645f7c0abccf6e4d14abf236a92993ded18395
Cunuduh <nickleodean.email3@gmail.com>
2023-02-09 01:30:31 -0500

    ae

--- commands/count.js
+++ commands/count.js
@@ -8,13 +8,13 @@ module.exports = {
         await message.delete()
         let previousMessage = await message.channel.fetchMessages({ limit: 1 })
         while (true) {
+            await new Promise(resolve => setTimeout(resolve, 3000))
             if (previousMessage.last().author.id !== client.user.id) {
                 const previousMessageNumber = previousMessage.last().content
                 const newMessageNumber = parseInt(previousMessageNumber) + 1
                 await message.channel.send(newMessageNumber)
                 previousMessage = await message.channel.fetchMessages({ limit: 1 })
             }
-            await new Promise(resolve => setTimeout(resolve, 3000))
         }
     }
 }
