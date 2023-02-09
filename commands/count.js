bb72d916704518602c050ddc164c6c6324cbd0c7
Cunuduh <nickleodean.email3@gmail.com>
2023-02-09 16:01:16 -0500

    update

--- commands/count.js
+++ commands/count.js
@@ -8,13 +8,14 @@ module.exports = {
         await message.delete()
         let previousMessage = await message.channel.fetchMessages({ limit: 1 })
         while (true) {
-            await new Promise(resolve => setTimeout(resolve, 3000))
+            await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 1000)))
             if (previousMessage.last().author.id !== client.user.id) {
                 const previousMessageNumber = previousMessage.last().content
                 const newMessageNumber = parseInt(previousMessageNumber) + 1
                 await message.channel.send(newMessageNumber)
                 previousMessage = await message.channel.fetchMessages({ limit: 1 })
             }
+            await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 250)))     
         }
     }
 }
