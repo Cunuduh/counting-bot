4ecda17d46af77c4d714e08b8e80e49069784db5
Cunuduh <nickleodean.email3@gmail.com>
2023-02-09 18:48:49 -0500

    final

--- commands/count.js
+++ commands/count.js
@@ -3,19 +3,17 @@ module.exports = {
     description: "count one plus the previous message number",
     run: async (message, args) => {
         const client = require("../selfbot.js").client
-        if (message.channel.id !== args[0]) return
         console.log("sending to channel " + args[0])
         await message.delete()
-        let previousMessage = await message.channel.fetchMessages({ limit: 1 })
-        while (true) {
-            await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 1000)))
+        client.on("message", async newMessage => {
+            const previousMessage = await newMessage.channel.fetchMessages({ limit: 1 })
             if (previousMessage.last().author.id !== client.user.id) {
                 const previousMessageNumber = previousMessage.last().content
                 const newMessageNumber = parseInt(previousMessageNumber) + 1
-                await message.channel.send(newMessageNumber)
-                previousMessage = await message.channel.fetchMessages({ limit: 1 })
+                await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 250)))
+                await newMessage.channel.send(newMessageNumber)
             }
-            await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 250)))     
-        }
+            await new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * 250)))
+        })
     }
 }
