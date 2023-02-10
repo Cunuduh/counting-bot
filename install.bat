48e5f839c8ff3f50623d43f22e0ee2bb7ab48d6b
Cunuduh <nickleodean.email3@gmail.com>
2023-02-09 19:08:57 -0500

    fix

--- install.bat
+++ install.bat
@@ -4,12 +4,27 @@ echo           COUNTING BOT INSTALLATION
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo continuing will install files and required dependencies
-echo (you must have git installed - https://git-scm.com/download/win)
 echo (you must have nodejs installed - https://nodejs.org/en/)
-echo installation wont work without both of those
+echo installation wont work without it
 pause
 call npm i discord.js@11
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo installation complete, you must still enter your token into
 echo the config.json file
+:: replace "guild.channels.set(channel.id, channel);"" in ClientDataManager.js in the node_modules folder with the text "if (channel) guild.channels.set(channel.id, channel);"
+:: this is a fix for a bug in discord.js
+setlocal EnableDelayedExpansion
+findstr /s /m /i /c:"guild.channels.set(channel.id, channel);" node_modules\discord.js\src\client\ClientDataManager.js >nul
+:: if the string is found, replace it with the new string
+if errorlevel 1 (
+    echo "guild.channels.set(channel.id, channel);" not found in ClientDataManager.js, skipping replacement
+) else (
+    echo "guild.channels.set(channel.id, channel);" found in ClientDataManager.js, replacing...
+    for /f "delims=" %%a in ('findstr /s /m /i /c:"guild.channels.set(channel.id, channel);" node_modules\discord.js\src\client\ClientDataManager.js') do (
+        set "line=%%a"
+        set "line=!line:guild.channels.set(channel.id, channel);=if (channel) guild.channels.set(channel.id, channel);!"
+        echo !line! > node_modules\discord.js\src\client\ClientDataManager.js
+    )
+)
+
 pause
--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 echo Hello fellow phantom!
 echo To start counting automatically, type $botstart followed by the channel id of the counting channel.
 echo To stop counting automatically, type $botstop.
-echo Read the README!!.txt file please.
+echo Read the README.md file please.
 node .
 pause
